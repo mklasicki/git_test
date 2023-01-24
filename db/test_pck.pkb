5d136eea0c000c92a2f80ac8cf82f782501a6b90
Marcin <klasicki.m@gmail.com>
2023-01-24 20:43:28 +0100

    fist commit

---
+++ db/test_pck.pkb
@@ -0,0 +1,10 @@
+create or replace package body test_pck is
+
+/*
+ * Marcin Klasicki DC-1000: test 001
+
+*/
+
+
+
+end test_pck;
---
+++ src/pl/marcin/Main.java
@@ -0,0 +1,8 @@
+package pl.marcin;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
